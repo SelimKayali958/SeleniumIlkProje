35f9846c4cf366ca3884064d4ce48e69f04f0017
SelimKayali958 <selimkayali958@gmail.com>
2020-08-20 23:26:13 +0300

    day04 ders sonu

---
+++ src/com/techproed/Day04_FindElements.java
@@ -0,0 +1,35 @@
+package com.techproed;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class Day04_FindElements {
+
+    public static void main(String[] args) {
+
+
+        System.setProperty("webdriver.chrome.driver","C:/Users/mk201/Documents/selenium dependencies/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10000, TimeUnit.MILLISECONDS);
+        driver.manage().window().maximize();
+        driver.get("http://a.testaddressbook.com/");
+
+        // a tagname sahip elemanlari bulalim
+
+        List<WebElement> listem = driver.findElements(By.tagName("a"));
+        for ( WebElement w : listem ) {
+            System.out.println(w.getText());
+        }
+
+
+
+
+    }
+
+
+}
